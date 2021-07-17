811f1289a11dd53096a33c2ef0bbca4257d052d1
sebz <bonillas175@gmail.com>
2021-07-16 23:55:22 -0400

    completion

--- Develop/script.js
+++ Develop/script.js
@@ -1,28 +1,47 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+var password = [''];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCase= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var specialCharacters = ['!', '#', '$', '%', '&', '*', '+', '/', '<', '=', '>', '?', '@', '~'];
+
 
 function generatePassword() {
-  // array of special characters
-  // array of numbers
-  // array of lower case letters
-  // array of upper case letters
-  // empty array of characters
-  //ask the user how many characters
-  //ask the user if they want numbers
-  //ask the user if they want special characters
-  //ask the user if they want uppercase letters
-  //ask the user if they want lower case letters
-  // if numbers, append numbers to character array
-  // if sc, append special characters to character array
-  // if ul, append upper case letetrs to characters array
-  // if ll, append lowercase letters
-  //generate the password
-  //repeat based on number of desired characters
-  //add random character from character array to password
-  //return the password
+
+var characterQuestion = parseInt(
+  window.prompt("How many characters would you like to include?")
+);
+if (characterQuestion === '') {
+  window.alert("Password length must be provided as a number");
+}
+if (characterQuestion < 8) {
+  window.alert("Minimum character count should be 8");
+}
+else if (characterQuestion > 128) {
+  window.alert("Maximum character count should be 128");
+}
+  var numberQuestion = window.confirm("Would you like to include numbers?");
+  var specialCharactersQuestion = window.confirm("Would you like to include special characters?");
+  var uppercaseQuestion = window.confirm("Would you like to include uppercase characters?");
+  var lowercaseQuestion = window.confirm("Would you like to include lower case characters?");
+
+  if (numberQuestion === true) {
+    password = password += numberQuestion;
+  }
+  if (specialCharactersQuestion === true) {
+    password = password += specialCharactersQuestion;
+  }
+  if (uppercaseQuestion === true) {
+    password = password += uppercaseQuestion;
+  }
+  if (lowercaseQuestion === true) {
+    password = password += lowercaseQuestion;
+  }
 }
 
-// Write password to the #password input
+generatePassword();
+
+var generateBtn = document.querySelector("#generate");
+
 function writePassword() {
   var password = generatePassword();
 
@@ -30,5 +49,5 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
